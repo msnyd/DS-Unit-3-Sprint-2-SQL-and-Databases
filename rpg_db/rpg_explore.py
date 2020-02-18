a4d1456349ea53767c25bd989344cdf70b067e65
msnyd <46577534+msnyd@users.noreply.github.com>
2020-02-18 15:30:21 -0600

    updates

--- rpg_db/rpg_explore.py
+++ rpg_db/rpg_explore.py
@@ -72,3 +72,61 @@ from
 GROUP BY character_id
 LIMIT 20
 """
+
+average_weapons_query = """
+
+   SELECT AVG(total_weapons)
+   FROM (
+    SELECT cc.name, COUNT(aw.power) as total_weapons
+    FROM charactercreator_character cc
+        LEFT JOIN charactercreator_character_inventory cci
+            ON cc.character_id=cci.character_id
+        LEFT JOIN armory_item ai ON cci.item_id=ai.item_id
+        LEFT JOIN armory_weapon aw ON ai.item_id=aw.item_ptr_id
+    GROUP BY cc.name)
+"""
+result = cursor.execute(average_weapons_query).fetchall()
+print(f'On average, each character has {result[0][0]:.2f} weapons')
+
+"""
+-- On average, how many Items does each Character have?
+-- row per character, two columns 1 char name, 2 item count
+-- for each character, how many items do they have
+-- any characters that don't have any items? 
+-- should we include them in our counts?
+-- row per char (302 rows)
+-- select *
+-- from charactercreator_character
+-- 898 rows (row per character per item)
+-- how many characters in the inventory table??????
+-- select count(distinct character_id) as char_count
+-- from charactercreator_character_inventory
+SELECT AVG(item_count) as avg_item_per_char -- 2.973
+FROM (
+    -- row per character
+    SELECT 
+     character_id
+     ,count(distinct item_id) as item_count
+    FROM charactercreator_character_inventory
+    GROUP BY character_id
+) subq
+-- select 898.0 / 302
+SELECT 
+  count(id) as row_count
+  ,count(distinct character_id) as char_count
+  ,count(id) / cast(count(distinct character_id) as float) as avg_item_per_char
+FROM charactercreator_character_inventory
+"""
+
+"""
+
+SELECT
+  c.character_id
+  ,inv.*
+FROM charactercreator_character c
+JOIN charactercreator_character_inventory inv ON inv.character_id = c.character_id
+"""
+
+"""
+
+"""
