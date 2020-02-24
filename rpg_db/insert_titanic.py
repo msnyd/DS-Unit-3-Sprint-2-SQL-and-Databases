2fccccb58441255f4d957329ae3ecb31da17f27d
msnyd <46577534+msnyd@users.noreply.github.com>
2020-02-24 11:07:35 -0600

    update

--- rpg_db/insert_titanic.py
+++ rpg_db/insert_titanic.py
@@ -21,9 +21,15 @@ class StorageService():
         self.sqlite_cursor = self.sqlite_connection.cursor()
         self.pg_connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
         self.pg_cursor = self.pg_connection.cursor()
+
+
     def get_sqlite_characters(self):
         return self.sqlite_connection.execute("SELECT * FROM charactercreator_character;").fetchall()
+
+
     def create_pg_characters_table(self):
+
+
         create_query = """
         DROP TABLE IF EXISTS characters; -- allows this to be run idempotently, avoids psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "characters_pkey" DETAIL:  Key (character_id)=(1) already exists.
         CREATE TABLE IF NOT EXISTS characters (
@@ -43,10 +49,14 @@ class StorageService():
         self.pg_cursor.execute(create_query)
         self.pg_connection.commit()
     def insert_pg_characters(self, characters):
+
+
         insertion_query = "INSERT INTO characters (character_id, name, level, exp, hp, strength, intelligence, dexterity, wisdom) VALUES %s"
         list_of_tuples = characters
         execute_values(self.pg_cursor, insertion_query, list_of_tuples)
         self.pg_connection.commit()
+
+        
 if __name__ == "__main__":
     service = StorageService()
     #
